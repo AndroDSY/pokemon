2c44338bde01b6fdfea36834ae94ef37d9747b4e
AndroDSY <andro@hispeed.ch>
2023-10-25 23:12:19 +0200

    added OCR

--- app/page.tsx
+++ app/page.tsx
@@ -1,14 +1,20 @@
 'use client'
 import { useState } from 'react'
 import Cards from '../components/Cards'
+import TextRecognition from '@/components/TextRecognition'
 
 export default function Page() {
   const [name, setName]:[name:string, setName:any] = useState('')
   const [count, setCount]:[count:number, setCount:any] = useState(5)
 
+  const changeName = (name:string) => {
+    setName(name)
+  }
+
   return(<>
-  <input placeholder='Pokemon Name' onInput={e => {setName(e.currentTarget.value)}}></input>
+  <input value={name} placeholder='Pokemon Name' onInput={e => {setName(e.currentTarget.value)}}></input>
   <input type='number' value={count} placeholder='Anzahl Pokemon' onInput={e => {setCount(e.currentTarget.value)}}></input>
+  <TextRecognition setName={changeName}/>
   <Cards name={name} count={count.toString()}></Cards>
   </>
   )
---
+++ components/TextRecognition.tsx
@@ -0,0 +1,39 @@
+import {useState, useRef} from 'react'
+import Webcam from 'react-webcam'
+import Tesseract from "tesseract.js"
+import style from '@/components/css/textRecognition.module.css'
+
+export default function TextRecognition({setName}:{setName:any}){
+
+    const webcamRef = useRef(null)
+    const [imgSrc, setImgSrc] = useState(null)
+
+    const detectName = () => {
+        // @ts-ignore
+        const imageSrc = webcamRef.current.getScreenshot();
+        Tesseract.recognize(
+            imageSrc,
+            'eng'
+        ).then(({ data: { text } }) => {
+            let nameArray = text.split('')
+            let filteredNameArray = nameArray.filter((character) => 
+                "abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ".split('').includes(character)
+            )
+            let name = filteredNameArray.join('')
+            setName(name)
+        })
+    }
+
+    return(
+        <>
+        <Webcam
+            audio={false}
+            ref={webcamRef}
+            screenshotFormat="image/jpeg"
+            className={style.webcam}
+        />
+
+        <button onClick={detectName}>Erkenne Pokémon</button>
+        </>
+    )
+}
