1840c5809a5894d5d2f8cf1abe8846621e1dbca3
AndroDSY <andro@hispeed.ch>
2023-10-26 20:08:31 +0200

    changed JSON structure - add base stats - added button for quicker inputs

--- app/page.tsx
+++ app/page.tsx
@@ -1,14 +1,19 @@
 'use client'
 import { useState } from 'react'
-import Cards from '../components/Cards'
+import Cards from '@/components/Cards'
+import style from '@/components/css/mainPage.module.css'
 
 export default function Page() {
   const [name, setName]:[name:string, setName:any] = useState('')
   const [count, setCount]:[count:number, setCount:any] = useState(5)
 
-  return(<>
-  <input placeholder='Pokemon Name' onInput={e => {setName(e.currentTarget.value)}}></input>
+  return(
+  <>
+  <input placeholder='Pokemon Name' id='nameInput' onInput={e => {setName(e.currentTarget.value)}}></input>
   <input type='number' value={count} placeholder='Anzahl Pokemon' onInput={e => {setCount(e.currentTarget.value)}}></input>
+  <br/>
+  {/*@ts-ignore*/}
+  <button className={style.inputSelector} onClick={() => {document.getElementById('nameInput').select()}}>Neue Eingabe</button>
   <Cards name={name} count={count.toString()}></Cards>
   </>
   )
