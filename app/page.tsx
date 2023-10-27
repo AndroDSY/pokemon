ff9a1e048919c1a72e1b4d245d2e17f52f0eeadc
AndroDSY <andro@hispeed.ch>
2023-10-28 01:09:53 +0200

    added functionality to predetermen count of pokemon

--- app/page.tsx
+++ app/page.tsx
@@ -1,11 +1,21 @@
 'use client'
 import { useState } from 'react'
+import { useSearchParams } from 'next/navigation'
 import Cards from '@/components/Cards'
 import style from '@/components/css/mainPage.module.css'
 
 export default function Page() {
+  
+  const search = useSearchParams()
+  let initialCount:any = null
+  if(search.get('anzahl')) {
+    initialCount = search.get('anzahl')
+  } else {
+    initialCount = 5
+  }
+  
   const [name, setName]:[name:string, setName:any] = useState('')
-  const [count, setCount]:[count:number, setCount:any] = useState(5)
+  const [count, setCount]:[count:number, setCount:any] = useState(initialCount)
 
   return(
   <>
